4a445f56c53171ab3d3fbaacd79753da681538cc
vini-j-pereira <viniciusjosepereira@gmail.com>
2025-01-31 22:12:40 -0300

    feat: render new Box in index

--- src/js/upBox.js
+++ src/js/upBox.js
@@ -4,6 +4,7 @@ let preco = document.getElementById("preco");
 let descricao = document.getElementById("descricao");
 
 let produtoBox = JSON.parse(localStorage.getItem("@newBox")) || [];
+carregarBox();
 
 function enviarBox(event){
     event.preventDefault();
@@ -25,9 +26,47 @@ function enviarBox(event){
     alert('Novo Box salvo com sucesso!')
 
     salvarBoxLocal();
+
+    carregarBox();
 }
 
 function salvarBoxLocal() {
 
     localStorage.setItem("@newBox", JSON.stringify(produtoBox));
+}
+
+function carregarBox(){
+    const containerBox = document.getElementById("containerBox");
+    containerBox.innerHTML = "",
+
+    produtoBox.map((item) => {
+        const card = document.createElement("div");
+        card.className = "box";
+
+        card.innerHTML = `
+                    <div class="box-header">
+                        <img src="${item.url}">
+                        <span>
+                            ${item.descri}
+                            <a class="ler-mais" href="#">Ler mais...</a>
+                        </span>
+                    </div>
+                    
+                    <div class="box-text">
+                        <h3>${item.titulo}</h3>
+                        
+                        <div class="buy">
+                            <p><strong>R$</strong>${item.preco}</p>
+                            <div class="btn-icon">
+                                <button>comprar</button>
+                                <a href="">
+                                    <i class="bi bi-basket"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+        `;
+
+        containerBox.appendChild(card);
+    })
 }

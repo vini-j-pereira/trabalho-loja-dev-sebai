ac5d1c08c15d5071292388134c0ac2a29fcf3174
vini-j-pereira <viniciusjosepereira@gmail.com>
2025-02-03 21:51:15 -0300

    feat: cometario funcition carregarBox

--- src/js/upBox.js
+++ src/js/upBox.js
@@ -40,15 +40,22 @@ function salvarBoxLocal() {
     localStorage.setItem("@newBox", JSON.stringify(produtoBox));
 }
 
+  //--------------------------------------------------------------------
+ //---> Função para renderizar o box dos livros na area de destaque da pagina:
+//Essa função bucas as informações do localStora e se hover algo no array = (produtoBox) ela renderiza na tela.
+
 function carregarBox(){
-    const containerBox = document.getElementById("containerBox");
-    containerBox.innerHTML = "",
+    const containerBox = document.getElementById("containerBox"); //Declaramos uma variavel para chamar a <div> da seção destaques do (HTML-linha 40)
+    containerBox.innerHTML = "", // Aqui nos aseguramos que o conteudo da varial containerBox estara vazio.
 
-    produtoBox.map((item) => {
-        const card = document.createElement("div");
-        card.className = "box";
+    produtoBox.map((item) => { // Nessa linha usamos o metodo (map) para mapear o array = produtoBox e avisamos que ele vai receber um parametro (item)
+        const card = document.createElement("div");// Declaramos uma varialvel e utilizamos o (createElement) para criar um elemto no caso uma <div> no nosso index.html
+        card.className = "box";//Nessa linha atribuimos uma classe a nossa <div> isso se faz necessario para depois chama-la no CSS 
 
-        card.innerHTML = `
+        //Nessas linhas a baixo estamos criando o proprio box dos livros em si
+        //card é a nossa variavel e o .innerHTML indica que queremos renderizar algo no HTML e dentro da aspas usamos HTML para montar nosso box de livro.
+        //Os elementos entre ${} vão receber os valores declarados dentro do nosso objeto onde (item) avisamos que queremos bucar um item de detro do objeto e depois falamos qua dos item(.url)  ou item(.descri) ou item(.titulo) ou item(.preco)
+        card.innerHTML = ` 
                     <div class="box-header">
                         <img src="${item.url}">
                         <span>
@@ -72,6 +79,6 @@ function carregarBox(){
                     </div>
         `;
 
-        containerBox.appendChild(card);
+        containerBox.appendChild(card); //Aqui indicamos que o nosso containerBox tem um apendice filho algo que vai dentro dele no caso o nosso (card);
     })
 }
